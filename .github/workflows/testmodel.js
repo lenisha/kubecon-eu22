786f79595a07b4865a498d6e904b2929b14701c7
Elena Neroslavskaya <lenisha@gmail.com>
2022-05-13 01:24:01 -0400

    Create testmodel.js

---
+++ .github/workflows/testmodel.js
@@ -0,0 +1,19 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  duration: '1m',
+  vus: 50,
+};
+
+export default function () {
+  const url = 'http://20.36.242.175/seldon/default/gpt2-model/v2/models/infer';
+  
+  
+  const blobStr = '{"inputs":[{"name":"huggingface","shape":[1],"datatype":"BYTES","data":["this is a test"]}]}';
+  
+  const res = http.post(url, payload, {
+          headers: { "Content-Type": "application/json" },
+  });
+  sleep(1);
+}
