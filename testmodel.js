ebc9bd25f36180ec9c13b9ad8bb15a7064158359
Elena Neroslavskaya <lenisha@gmail.com>
2022-05-13 01:34:06 -0400

    add k6

--- testmodel.js
+++ testmodel.js
@@ -10,7 +10,7 @@ export default function () {
   const url = 'http://20.36.242.175/seldon/default/gpt2-model/v2/models/infer';
   
   
-  const blobStr = '{"inputs":[{"name":"huggingface","shape":[1],"datatype":"BYTES","data":["this is a test"]}]}';
+  const payload = '{"inputs":[{"name":"huggingface","shape":[1],"datatype":"BYTES","data":["this is a test"]}]}';
   
   const res = http.post(url, payload, {
           headers: { "Content-Type": "application/json" },
