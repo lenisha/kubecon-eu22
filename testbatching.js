204712a6f4e914a5254c1305fc0b94c8e9cc0276
Elena Neroslavskaya <lenisha@gmail.com>
2022-05-18 02:01:28 -0400

    add batch

--- testbatching.js
+++ testbatching.js
@@ -12,12 +12,25 @@ export const options = {
       maxVUs: 100,
       exec: 'gpu_test_batching',
       tags: { my_custom_tag: 'gpu_test_batching' },
+      env: { INFER_URL: 'http://20.36.242.175/seldon/default/gpt2-gpu-batch/v2/models/infer' },
+    },
+    gpu_test: {
+      executor: 'constant-arrival-rate',
+      rate: 128,
+      timeUnit: '1s',
+      duration: '30s',
+      preAllocatedVUs: 10,
+      maxVUs: 100,
+      exec: 'gpu_test',
+      tags: { my_custom_tag: 'gpu_test' },
       env: { INFER_URL: 'http://20.36.242.175/seldon/default/gpt2-gpu/v2/models/infer' },
     }
   },
   thresholds: {
     'iteration_duration{scenario:gpu_test_batching}': [`max>=0`],
     'http_req_duration{scenario:gpu_test_batching}': [`max>=0`],
+    'iteration_duration{scenario:gpu_test}': [`max>=0`],
+    'http_req_duration{scenario:gpu_test}': [`max>=0`],
   },
 };
 
@@ -40,9 +53,26 @@ export function gpu_test_batching() {
 
   check (res, {
     'status is 200': (r) => r.status == 200,
-    'has text': (r) => r.body.includes('generated'),
+    
   }, { type: 'read' });
 
 }
+
+export function gpu_test() {
+  
+  const res = http.post(__ENV.INFER_URL, payload, {
+          headers: { "Content-Type": "application/json" },
+          timeout: "300s",
+  });
+
+  check (res, {
+    'status is 200': (r) => r.status == 200,
+    
+  }, { type: 'read' });
+
+}
+
+
+
 
 
